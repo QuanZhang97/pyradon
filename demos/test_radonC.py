e705d298d6fa4f24e580b6d499ae909b1d6327b6
Yangkang Chen <chenyk1990@gmail.com>
2022-10-06 02:14:35 -0500

    add C version

---
+++ demos/test_radonC.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#import io
+import numpy as np
+import pyradon as pr
+#import scipy
+# import radon_op
+import matplotlib.pyplot as plt
+# import pcg
+# 
+# REFERENCE
+# Chen, 2018, GEO, Automatic velocity analysis using high-resolution hyperbolic Radon transform
+# Zhang, 2022, GJI, Improving receiver function with high-resolution Radon transform
+
+
+d = np.loadtxt('./data/test_z_data.txt')
+h = np.loadtxt('./data/test_dist_data.txt')
+
+nh = len(h)
+dq = 0.005
+q = np.arange(-2,2,dq)
+nq = len(q)
+
+t = np.linspace(-5.0,144.9,1500)
+dt = 0.1
+nt = len(t)
+#m = np.zeros((nt,nq))
+
+Param = {'h':h,'v':1./q,'q':q,'nt':nt,'dt':dt,'typ':1};
+
+ma=np.zeros((nt,nq))
+
+nx = len(d)
+
+N1 = 10  # CG Iterations (Internal loop);
+N2 = 1   # Update of weights for the sparse solution: N1 = 1 LS;  N2 > 3 for High Res (Sparse) solution
+
+print("radon_l2c begins\n")
+mil2,misfitl2 = pr.radon_l2c(Param,d,np.zeros(ma.shape),niter=N1,verb=1)
+print("radon_l2c is done\n")
+
+print("radon_l1c begins\n")
+mil1,misfitl1 = pr.radon_l1c(Param,d,np.zeros(ma.shape),niter_in=N1,niter_out=3,verb=1)
+print("radon_l1c is done\n")
+
+# 
+# 
+# print("radon_l2 begins\n")
+# mi2,misfit = pr.radon_l2(Param,d,np.zeros(ma.shape),niter=N1,verb=1)
+# print("radon_l2 is done\n")
+# 
+# ## Adjoint
+# m = pr.radon(d,Param,-1)
+# print("Adjoint is done\n")
+# ## Inversion
+# mi,misfit = pr.pcg(pr.radon,Param,d,np.zeros(ma.shape),niter_in=N1,niter_out=N2,verb=1)
+# print("Inversion is done\n")
+# di = pr.radon(mi,Param,1)
+
+plt.figure(figsize=(14,16)) # Plot original data with Radon spectrum (d,m)
+plt.subplot(1,2,1)
+plt.imshow(mil2)
+plt.subplot(1,2,2)
+plt.imshow(mil1)
+plt.show()
+
+m=mil2;
+plt.figure(figsize=(14,16)) # Plot original data with Radon spectrum (d,m)
+plt.subplot(1,2,1)
+d_max=np.max(d[:,10])/10
+for n in range(np.size(d,1)):
+    x=d[:,n]
+    plt.plot(x/d_max+h[n-1],t,'k')
+ax=plt.gca()
+plt.xlim(np.min(h),np.max(h))
+plt.ylim(np.min(t),np.max(t))
+ax.invert_yaxis()
+plt.xlabel('Distance(km)',fontdict={'weight':'normal','size': 20})
+plt.ylabel('Time (s)',fontdict={'weight':'normal','size': 20})
+plt.tick_params(labelsize=20)
+plt.title('RFs',fontdict={'weight':'normal','size': 20})
+
+plt.subplot(1,2,2)
+plt.imshow(m/np.max(m),cmap='RdBu',clim=(-0.2,0.2),extent=[q.min(),q.max(),t.max(),t.min()],interpolation='nearest',aspect='auto')
+#plt.xlim(0,3200)
+#plt.ylim(1500,0)
+ax=plt.gca()
+ax.yaxis.set_ticks_position('right')
+plt.xlabel('Slowness (s/km)',fontdict={'weight':'normal','size': 20})
+#plt.ylabel('Time (s)',fontdict={'weight':'normal','size': 20})
+plt.title('Radon Spectrum',fontdict={'weight':'normal','size': 20})
+plt.tick_params(labelsize=20)
+#plt.colorbar(extend='both',orientation='horizontal')
+plt.savefig('test.png',bbox_inches='tight',dpi=300)
+
+
+# 
+# plt.figure(figsize=(14,16)) # Plot Radon denoised data with Radon spectrum (di,mi)
+# plt.subplot(1,2,1)
+# di_max=np.max(di[:,10])/10
+# for n in range(np.size(di,1)):
+#     x=di[:,n]
+#     plt.plot(x/di_max+h[n-1],t,'k')
+# ax=plt.gca()
+# plt.xlim(np.min(h),np.max(h))
+# plt.ylim(np.min(t),np.max(t))
+# ax.invert_yaxis()
+# plt.xlabel('Distance(km)',fontdict={'weight':'normal','size': 20})
+# plt.ylabel('Time (s)',fontdict={'weight':'normal','size': 20})
+# plt.tick_params(labelsize=20)
+# plt.title('RFs',fontdict={'weight':'normal','size': 20})
+# 
+# plt.subplot(1,2,2)
+# plt.imshow(mi/np.max(mi),cmap='RdBu',clim=(-0.2,0.2),extent=[q.min(),q.max(),t.max(),t.min()],interpolation='nearest',aspect='auto')
+# #plt.xlim(0,3200)
+# #plt.ylim(1500,0)
+# ax=plt.gca()
+# ax.yaxis.set_ticks_position('right')
+# plt.xlabel('Slowness (s/km)',fontdict={'weight':'normal','size': 20})
+# #plt.ylabel('Time (s)',fontdict={'weight':'normal','size': 20})
+# plt.title('Radon Spectrum',fontdict={'weight':'normal','size': 20})
+# plt.tick_params(labelsize=20)
+# #plt.colorbar(extend='both',orientation='horizontal')
+# plt.savefig('test_i.png',bbox_inches='tight',dpi=300)
---
+++ demos/test_syn.py
@@ -0,0 +1,167 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyradon as pr
+import time
+
+def ricker(f,dt,tlength=None):
+    # ricker: Ricker wavelet of central frequency f.
+    #
+    # INPUT:
+    # f : central freq. in Hz (f <<1/(2dt) )
+    # dt: sampling interval in sec
+    # tlength : the duration of wavelet in sec
+    #
+    # OUTPUT: 
+    # w:  the Ricker wavelet
+    # tw: time axis
+    #
+    # Example
+    #
+    #   [w,tw] = ricker(10,0.004,0.2);
+    #   pyplot.plot(tw,w);
+    import numpy as np
+    
+    if tlength!=None:
+        nw=np.floor(tlength/dt)+1;
+    else:
+        nw=2.2/f/dt;
+        nw=2*np.floor(nw/2)+1;
+    nc=np.floor(nw/2);
+    nw=int(nw)
+    w =np.zeros(nw);
+    
+    k=np.arange(1,nw+1,1);
+    alpha = (nc-k+1)*f*dt*np.pi;
+    beta=np.power(alpha,2);
+    w = (1.-beta*2)*np.exp(-beta);
+    tw = -(nc+1-k)*dt;
+    return w,tw
+
+dp=0.05;dh=0.1;
+
+h=np.linspace(0,6,int(6/dh+1));
+
+nh=len(h);
+p=np.linspace(-0.6,0.60,int(1.21/dp)+1);
+nnp=len(p);
+
+dt=0.2;nt=501;
+m=np.zeros([nt,nnp]);
+d=np.zeros([nt,nh]);
+w,tw=ricker(0.5,dt);
+
+
+tau=[100,200,250];
+p0=[0,0.1,0.1];
+
+
+m[tau[0]-1,12]=1;
+m[tau[1]-1,18]=0.4;
+m[tau[2]-1,18]=0.5;
+# t = np.convolve(m[:,1],w,'same');
+import scipy.signal
+w=np.expand_dims(w,1);
+m=scipy.signal.convolve2d(m,w,'same');
+
+par={'h':h,'v':1/(p+0.0000000000000001),'nt':nt,'dt':dt,'typ':1}
+
+#Forward operator
+
+
+
+
+## Python version
+# tic = time.perf_counter()
+d=pr.radon(m,par,1);
+# #Adjoint operator
+# ma = pr.radon(d,par,-1);
+# toc = time.perf_counter()
+# print(f"Python version takes {toc - tic:0.4f} seconds");
+# 
+# plt.figure(figsize=(8, 8));
+# plt.subplot(1,3,1)
+# plt.imshow(m,aspect='auto');
+# plt.subplot(1,3,2)
+# plt.imshow(d,aspect='auto');
+# plt.subplot(1,3,3)
+# plt.imshow(ma,aspect='auto');
+# plt.show()
+# 
+# 
+# ## C version
+tic = time.perf_counter()
+### C-version
+#Forward operator
+d_c=pr.radonc(m,par,1);
+#Adjoint operator
+ma_c = pr.radonc(d_c,par,-1);
+toc = time.perf_counter()
+print(f"C version takes {toc - tic:0.4f} seconds");
+# 
+# plt.figure(figsize=(8, 8));
+# plt.subplot(1,3,1)
+# plt.imshow(m,aspect='auto');
+# plt.subplot(1,3,2)
+# plt.imshow(d_c,aspect='auto');
+# plt.subplot(1,3,3)
+# plt.imshow(ma_c,aspect='auto');
+# plt.show()
+# 
+# ## Comparison
+# plt.figure(figsize=(8, 8));
+# plt.imshow(np.concatenate((ma,ma_c,ma-ma_c),axis=1),aspect='auto');
+# plt.show()
+# 
+
+## LS
+# tic = time.perf_counter()
+# mi=pr.radon_l2(par,d,np.zeros(m.shape),niter=10,verb=1)
+# toc = time.perf_counter()
+# print(f"Python version LS takes {toc - tic:0.4f} seconds");
+
+tic = time.perf_counter()
+mi_c=pr.radon_l2c(par,d,np.zeros(m.shape),niter=10,verb=1)[0]
+toc = time.perf_counter()
+print(f"C version LS takes {toc - tic:0.4f} seconds");
+
+# plt.figure(figsize=(8, 8));
+# plt.imshow(np.concatenate((mi,mi_c,mi-mi_c),axis=1),aspect='auto');
+# plt.show()
+
+
+## High-resolution 
+# tic = time.perf_counter()
+# ml1=pr.radon_l1(par,d,np.zeros(m.shape),niter_in=10,niter_out=3,verb=1)
+# toc = time.perf_counter()
+# print(f"Python version L1 takes {toc - tic:0.4f} seconds");
+# 
+tic = time.perf_counter()
+ml1_c=pr.radon_l1c(par,d,np.zeros(m.shape),niter_in=10,niter_out=3,verb=1)[0]
+toc = time.perf_counter()
+print(f"C version L1 takes {toc - tic:0.4f} seconds");
+# 
+# plt.figure(figsize=(8, 8));
+# plt.imshow(np.concatenate((ml1,ml1_c,ml1-ml1_c),axis=1),aspect='auto');
+# plt.show()
+
+
+plt.figure(figsize=(8, 8));
+plt.subplot(1,5,1)
+plt.imshow(m,aspect='auto');plt.title('Model');
+plt.subplot(1,5,2)
+plt.imshow(d,aspect='auto');plt.title('Data');plt.gca().set_yticks([]);
+plt.subplot(1,5,3)
+plt.imshow(ma_c,aspect='auto');plt.title('Adjoint');plt.gca().set_yticks([]);
+plt.subplot(1,5,4)
+plt.imshow(mi_c,aspect='auto');plt.title('LS inversion');plt.gca().set_yticks([]);
+plt.subplot(1,5,5)
+plt.imshow(ml1_c,aspect='auto');plt.title('L1 inversion');plt.gca().set_yticks([]);
+plt.show()
+
+
+
+
+
+
+
+
